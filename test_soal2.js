4e0eba62ffd03c0248959df09bc6805dee38fd9a
heriryan23 <44110909+heriryan23@users.noreply.github.com>
2022-07-11 23:06:22 +0800

    Heri Rian Muliadi : Test Kawah Edukasi batch III

---
+++ test_soal2.js
@@ -0,0 +1,12 @@
+function validation() {
+    var validasiHuruf = /^[a-zA-Z ]+$/;
+    var nama = document.getElementById("nama");
+    if (nama.value.match(validasiHuruf)) {
+        alert("Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan hallo " + nama.value + " selamat datang!");
+    } else {
+        alert("Sistem kami menolak untuk inputan berisi angka");
+        nama.value = "";
+        nama.focus();
+        return false;
+    }
+}
