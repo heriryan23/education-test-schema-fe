68aa1eb8849dfedbe0951128c575141604b2fd0d
heriryan23 <44110909+heriryan23@users.noreply.github.com>
2022-07-13 15:14:45 +0800

    HERI RIAN MULIADI : Test Kawah Edukasi batch III

--- test_soal1.java
+++ test_soal1.java
@@ -1,12 +1,27 @@
+import java.util.Arrays;
+
 class data {
     public static void main(String[] args) {
-        int[] data1 = { 1, 1, 1, 2, 2, 4, 1, 1 };
-        int x = 1;
-        int Count = 0;
-        for (int i = 0; i < data1.length; i++) {
-            if (data1[i] == x)
-                Count++;
+        int[] data1 = { 1, 1, 1, 2, 2, 4, 1, 1, 2, 1, 2, 2, 2, 8, 1, 1, 3, 3, 3, 3, 2, 4, 1, 1 };
+        Total(data1);
+    }
+
+    static void Total(int data1[]) {
+        Arrays.sort(data1);
+        for (int i = 0; i < data1.length - 1; i++) {
+            int count = 1;
+            for (int j = i + 1; j < data1.length; j++) {
+                if (data1[i] == data1[j])
+                    count++;
+                else
+                    break;
+
+            }
+            if (count > 1) {
+                System.out.println(
+                        "total data paling banyak keluar adalah angka " + data1[i] + " dengan -> " + "jumlah " + count);
+                i += (count - 1);
+            }
         }
-        System.out.println("total data paling banyak keluar adalah angka " + x + " dengan jumlah " + Count);
     }
 }
